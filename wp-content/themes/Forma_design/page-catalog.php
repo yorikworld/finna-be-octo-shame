f0d199fe81a29536b67a1c73c16130c4f4faf2cd
yorikworld <yorikworld@gmail.com>
2014-07-09 01:39:16 +0300

    modifed ignore,forma.sql2. Taxonomy ready.

--- wp-content/themes/Forma_design/page-catalog.php
+++ wp-content/themes/Forma_design/page-catalog.php
@@ -7,87 +7,21 @@ get_header()?>
             <div class="wrapper">
                 <div class="page-all">
                     <div class="sidebar">
-                        <div class="box-sex">
-                            <h3><a href="#">Женская</a></h3>
-                            <ul>
-                                <li><a href="#">весна</a></li>
-                                <li><a href="#">осень</a></li>
-                                <li><a href="#">зима</a></li>
-                            </ul>
-                        </div>
-                        <div class="box-sex">
-                            <h3><a href="#">Мужская</a></h3>
-                            <ul>
-                                <li><a href="#">весна</a></li>
-                                <li><a href="#">осень</a></li>
-                                <li><a href="#">зима</a></li>
-                            </ul>
-                        </div>
+                        <?php $hiterms = get_terms("genre", array("orderby" => "id", "parent" => 0, "hide_empty" => false)); ?>
+                        <?php foreach ($hiterms as $key => $hiterm) : ?>
+                            <div class="box-sex">
+                                <h3><a href="<?php echo get_term_link( $hiterm, "genre");?>"><?php echo $hiterm->name; ?></a></h3>
+                                <?php $loterms = get_terms("genre", array("orderby" => "slug", "parent" => $hiterm->term_id, "hide_empty" => false)); ?>
+                                <?php if ($loterms) : ?>
+                                    <ul>
+                                        <?php foreach ($loterms as $key => $loterm) : ?>
+                                            <li><a href="<?php echo get_term_link( $loterm, "genre");?>"><?php echo $loterm->name; ?></a></li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                <?php endif; ?>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
-                    <?php
-                    $args = array(
-                        'orderby'       => 'term_group',
-                        'order'         => 'ASC',
-                        'hide_empty'    => false,
-                        'exclude'       => array(),
-                        'exclude_tree'  => array(),
-                        'include'       => array(),
-                        'number'        => '',
-                        'fields'        => 'all',
-                        'slug'          => '',
-                        'parent'        => 0,
-                        'hierarchical'  => true,
-                        'child_of'      => 0,
-                        'get'           => '',
-                        'name__like'    => '',
-                        'pad_counts'    => false,
-                        'offset'        => '',
-                        'search'        => '',
-                        'cache_domain'  => 'core'
-                    );
-                    $args2 = array(
-                        'orderby'       => 'term_group',
-                        'order'         => 'ASC',
-                        'hide_empty'    => false,
-                        'exclude'       => array(),
-                        'exclude_tree'  => array(),
-                        'include'       => array(),
-                        'number'        => '',
-                        'fields'        => 'all',
-                        'slug'          => '',
-                        'parent'        => '',
-                        'hierarchical'  => true,
-                        'child_of'      => 0,
-                        'get'           => '',
-                        'name__like'    => '',
-                        'pad_counts'    => false,
-                        'offset'        => '',
-                        'search'        => '',
-                        'cache_domain'  => 'core'
-                    );
-
-                    $myterms = get_terms( 'genre', $args );
-                    // $myterms = get_terms( 'post_tag', $args );
-                    $i = 0;
-                    foreach( $myterms as $term )
-                    {
-                        echo '<pre>';
-//                        var_dump($term);
-                       echo($term->name);
-                        echo '</pre>';
-                            $myterms2 = get_terms( 'genre', array("parent" => $term ->term_id));
-                            foreach( $myterms2 as $term2 )
-                            {
-                                echo '<pre>';
-                            echo($term->name);
-                                echo '</pre>';
-                            }
-                        echo '</pre>';
-//                        echo('<br>');
-                    }
-                    ?>
-
-
                     <div class="content-main">
                         <ul class="catalog">
                             <li><a href="#"><img src="<?php echo get_bloginfo( 'stylesheet_directory') ?>/img/catalog-pic.png" height="288" width="177" alt="" /></a></li>
---
+++ wp-content/themes/Forma_design/taxonomy.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yorik
+ * Date: 09.07.14
+ * Time: 1:14
+ */
+echo '123';
